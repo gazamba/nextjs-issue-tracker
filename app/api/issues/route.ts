9ef8b31d967e637c9937cce7df4c0499de925723
I504428 <gabriel.azambuja@sap.com>
2024-09-22 17:28:10 -0300

    feat: build an api for creating issues

---
+++ app/api/issues/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/client";
+import zod from "zod";
+
+const newIssueSchema = zod.object({
+  title: zod.string().min(3),
+  description: zod.string(),
+});
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = newIssueSchema.safeParse(body);
+
+  if (!validation.success) {
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  }
+
+  const newIssue = await prisma.issue.create({
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(newIssue, { status: 201 });
+}

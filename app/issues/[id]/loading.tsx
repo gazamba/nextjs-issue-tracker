93e29af011682c059c0056bbdcd6ccc1265d4fff
I504428 <gabriel.azambuja@sap.com>
2024-09-24 13:57:28 -0300

    feat(ui) : show feat details

---
+++ app/issues/[id]/loading.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoadingUserDetailPage = () => {
+  return <div>Loading...</div>;
+};
+
+export default LoadingUserDetailPage;
---
+++ app/issues/new/loading.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoadingUserNewPage = () => {
+  return <div>Loading...</div>;
+};
+
+export default LoadingUserNewPage;

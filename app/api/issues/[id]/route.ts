0d047431d0befc4be2409841d5df415e46ac3f42
I504428 <gabriel.azambuja@sap.com>
2024-09-26 09:53:17 -0300

    feat: build delete api

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -18,7 +18,7 @@ export async function PATCH(
   });
 
   if (!issue) {
-    NextResponse.json({ error: "Issue not found" }, { status: 404 });
+    NextResponse.json({ message: "Issue not found" }, { status: 404 });
   }
 
   const updatedIssue = await prisma.issue.update({
@@ -30,4 +30,26 @@ export async function PATCH(
   });
 
   return NextResponse.json(updatedIssue, { status: 200 });
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue) {
+    return NextResponse.json({ message: "Issue not found" }, { status: 404 });
+  }
+
+  await prisma.issue.delete({
+    where: { id: parseInt(params.id) },
+  });
+
+  return NextResponse.json(
+    { message: "Issue deleted successfully" },
+    { status: 200 }
+  );
 }

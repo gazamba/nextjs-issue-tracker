c0884944fb12bfd12d819cb5475e1612c4cbb559
I504428 <gabriel.azambuja@sap.com>
2024-09-26 15:21:09 -0300

    feat: improve ux

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,17 +1,40 @@
 "use client";
 
-import { AlertDialog, Button, Flex } from "@radix-ui/themes";
+import { AlertDialog, Flex, Button, Box } from "@radix-ui/themes";
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
+import { Spinner } from "@/app/components";
+import "@/app/globals.css";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
+  const [error, setError] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  const deleteIssue = async () => {
+    try {
+      setIsDeleting(true);
+      await axios.delete("/api/issues/" + issueId);
+      router.push("/issues");
+      router.refresh();
+    } catch (error) {
+      setIsDeleting(false);
+      setError(true);
+    }
+  };
+
   return (
     <>
       <AlertDialog.Root>
         <AlertDialog.Trigger>
-          <Button color="red">Delete Issue</Button>
+          <Button
+            style={{ cursor: "pointer" }}
+            disabled={isDeleting}
+            color="red"
+          >
+            <Box>Delete Issue {isDeleting && <Spinner />}</Box>
+          </Button>
         </AlertDialog.Trigger>
         <AlertDialog.Content maxWidth="450px">
           <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
@@ -26,12 +49,8 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
             </AlertDialog.Cancel>
             <AlertDialog.Action>
               <Button
-                onClick={async () => {
-                  console.log(issueId);
-                  await axios.delete("/api/issues/" + issueId);
-                  router.push("/issues");
-                  router.refresh();
-                }}
+                style={{ cursor: "pointer" }}
+                onClick={deleteIssue}
                 variant="solid"
                 color="red"
               >
@@ -39,6 +58,26 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
               </Button>
             </AlertDialog.Action>
           </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+
+      <AlertDialog.Root open={error}>
+        <AlertDialog.Content maxWidth="450px">
+          <AlertDialog.Title>Error</AlertDialog.Title>
+          <AlertDialog.Description size="2">
+            This issue could not be deleted. Please try again.
+          </AlertDialog.Description>
+          <AlertDialog.Cancel>
+            <Button
+              style={{ cursor: "pointer" }}
+              mt="2"
+              onClick={() => setError(false)}
+              variant="soft"
+              color="gray"
+            >
+              OK
+            </Button>
+          </AlertDialog.Cancel>
         </AlertDialog.Content>
       </AlertDialog.Root>
     </>
--- app/issues/[id]/EditIssueButton.tsx
+++ app/issues/[id]/EditIssueButton.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const EditIssueButton = ({ issueId }: { issueId: number }) => {
   return (
     <>
-      <Button>
+      <Button style={{ cursor: "pointer" }}>
         <Pencil2Icon />
         <Link href={`/issues/${issueId}/edit`}>Edit Issue</Link>
       </Button>

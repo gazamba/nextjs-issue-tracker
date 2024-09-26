f0f081cbf69b6c0d78de0b4a76b5074c242769d5
I504428 <gabriel.azambuja@sap.com>
2024-09-26 20:47:09 -0300

    feat: add prisma adapter

--- app/auth.ts
+++ app/auth.ts
@@ -1,5 +1,15 @@
 import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@/prisma/client";
+import { env } from "process";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Google({
+      clientId: env.GOOGLE_CLIENT_ID!,
+      clientSecret: env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
 });

03e7787cbc67ca16361bbf32bb5c7abbca828d57
I504428 <gabriel.azambuja@sap.com>
2024-09-27 08:07:53 -0300

    feat: add loadig skeleton to login nav

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Skeleton from "@/app/components/Skeleton";
 import React from "react";
 import Link from "next/link";
 import { AiFillBug } from "react-icons/ai";
@@ -63,7 +64,7 @@ const NavLinks = () => {
 const AuthStatus = () => {
   const { status, data: session } = useSession();
 
-  if (status === "loading") return null;
+  if (status === "loading") return <Skeleton width="2rem" />;
 
   if (status === "unauthenticated") {
     return (

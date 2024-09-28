08e183bfa595fe4339f4d93f976797180011eaad
I504428 <gabriel.azambuja@sap.com>
2024-09-27 21:55:28 -0300

    feat: assign issue to user

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -2,19 +2,19 @@
 
 import React, { useEffect, useState } from "react";
 import { Select } from "@radix-ui/themes";
-import { User } from "@prisma/client";
+import { Issue, User } from "@prisma/client";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 import Skeleton from "@/app/components/Skeleton";
 
-const AssigneeSelect = () => {
+const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const {
     data: users,
     error,
     isLoading,
   } = useQuery<User[]>({
     queryKey: ["users"],
-    queryFn: () => axios.get("/xapi/users").then((res) => res.data),
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
     staleTime: 1000 * 60,
     retry: 3,
   });
@@ -24,13 +24,27 @@ const AssigneeSelect = () => {
   if (error) return null;
 
   return (
-    <Select.Root>
-      <Select.Trigger placeholder="Assign..." />
+    <Select.Root
+      defaultValue={issue.assignedUserId || "unassigned"}
+      onValueChange={(userId) => {
+        axios.patch("/api/issues/" + issue.id, {
+          assignedUserId: userId === "unassigned" ? null : userId,
+        });
+      }}
+    >
+      <Select.Trigger className="cursor-pointer" placeholder="Assign..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
+          <Select.Item className="cursor-pointer" value="unassigned">
+            Unassigned
+          </Select.Item>
           {users?.map((user) => (
-            <Select.Item key={user.id} value={user.id}>
+            <Select.Item
+              className="cursor-pointer"
+              key={user.id}
+              value={user.id}
+            >
               {user.name}
             </Select.Item>
           ))}
